13c8be02def74a35ee3f8156e8679116288d2a95
gotofritz <gotofritz@users.noreply.github.com>
2022-03-13 21:32:57 +0100

    python manage.py makemigrations recipes

--- nutrisho/recipes/models.py
+++ nutrisho/recipes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Cuisine(models.Model):
+    cuisine = models.CharField(
+        "region.area.subarea just as placeholder", max_length=200
+    )
+
+
+class Recipe(models.Model):
+    name = models.CharField("Recipe main name", max_length=200)
+    short_description = models.CharField(
+        "A short blurb about the recipe", max_length=200, null=True
+    )
+    source = models.CharField(
+        "The URL or book where the recipe came from", max_length=200, null=True
+    )
+    cuisine = models.ForeignKey(Cuisine, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField("date created")
+
+
+class Tag(models.Model):
+    tag = models.CharField("Name of tag", max_length=64)
+    recipe = models.ManyToManyField(Recipe)
