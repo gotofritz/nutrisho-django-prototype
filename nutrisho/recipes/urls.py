54a7061b719f22a16fc0daf79d5a80a354e2614d
gotofritz <gotofritz@users.noreply.github.com>
2022-03-25 00:43:17 +0100

    wip: add url for recipe edit

--- nutrisho/recipes/urls.py
+++ nutrisho/recipes/urls.py
@@ -6,4 +6,5 @@ app_name = "recipes"
 urlpatterns = [
     path("", home, name="home"),
     path("<int:recipe_id>/", recipe, name="recipe"),
+    path("<int:recipe_id>/edit", recipe, name="edit"),
 ]
