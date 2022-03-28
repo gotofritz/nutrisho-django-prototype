84f38b73d1ed45e2e79a8c7bcd5fb5503a08aebe
gotofritz <gotofritz@users.noreply.github.com>
2022-03-29 00:33:03 +0200

    added: navigate to next/prev with new svg icons

--- nutrisho/recipes/views/recipe.py
+++ nutrisho/recipes/views/recipe.py
@@ -6,14 +6,31 @@ from recipes.models import Recipe
 from recipes.forms.recipe_edit_form import RecipeEditForm
 
 
+def _get_recipe_nav(recipe_id):
+    order_by = "id"
+    try:
+        prev_id = Recipe.objects.filter(id__lt=recipe_id).order_by(f"-{order_by}")[0].id
+    except IndexError:
+        prev_id = Recipe.objects.all().order_by(f"-{order_by}")[0].id
+    try:
+        next_id = Recipe.objects.filter(id__gt=recipe_id).order_by(order_by)[0].id
+    except IndexError:
+        next_id = Recipe.objects.all().order_by(order_by)[0].id
+    return {"prev_id": prev_id, "next_id": next_id}
+
+
 def recipe(request, recipe_id):
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
-    return render(request, "recipes/recipe.html", {"recipe": recipe})
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(
+        request,
+        "recipes/recipe.html",
+        {"recipe": recipe, "nav": _get_recipe_nav(recipe_id)},
+    )
 
 
 def recipe_edit(request, recipe_id):
     debug = {}
-    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    recipe = get_object_or_404(Recipe, id=recipe_id)
     form_data = {}
     if request.POST:
         debug = "YES"
@@ -27,8 +44,14 @@ def recipe_edit(request, recipe_id):
             "short_description": recipe.short_description,
         }
     form = RecipeEditForm(form_data, instance=recipe)
+
     return render(
         request,
         "recipes/recipe.html",
-        {"recipe": recipe, "form": form, "debug": debug},
+        {
+            "recipe": recipe,
+            "form": form,
+            "debug": debug,
+            "nav": _get_recipe_nav(recipe),
+        },
     )
